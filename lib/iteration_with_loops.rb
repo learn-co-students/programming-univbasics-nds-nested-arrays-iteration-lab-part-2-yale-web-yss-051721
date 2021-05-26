ba1fda55bcc12f937eee5ae32db5effd8a040489
clean-loop-a64b <dsjong2002@yahoo.com>
2021-05-26 07:04:22 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,13 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  ans = []
+  src.each do |a|
+    mini = a[0]
+    a.each do |x|
+      mini = x < mini ? x : mini
+    end
+    ans += [mini]
+  end
+  return ans
 end
